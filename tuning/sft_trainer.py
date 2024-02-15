01bcf8bda0c467edb408a4b2a39ebbaf371241c5
Padmanabha V Seshadri <seshapad@in.ibm.com>
2024-02-15 00:44:21 -0500

    feat: Policy driven training control
    Signed-off-by: Padmanabha V Seshadri <seshapad@in.ibm.com>

--- tuning/sft_trainer.py
+++ tuning/sft_trainer.py
@@ -27,7 +27,7 @@ from tuning.config import configs, peft_config
 from tuning.data import tokenizer_data_utils
 from tuning.utils.config_utils import get_hf_peft_config
 from tuning.utils.data_type_utils import get_torch_dtype
-
+from tuning.policydrivencontroller import PolicyDrivenTrainerControl
 
 class PeftSavingCallback(TrainerCallback):
     def on_save(self, args, state, control, **kwargs):
@@ -88,6 +88,7 @@ def train(
     model_args: configs.ModelArguments,
     data_args: configs.DataArguments,
     train_args: configs.TrainingArguments,
+    train_control_args: configs.PTCArguments,
     peft_config: Optional[
         Union[peft_config.LoraConfig, peft_config.PromptTuningConfig]
     ] = None,
@@ -98,6 +99,7 @@ def train(
         model_args: tuning.config.configs.ModelArguments
         data_args: tuning.config.configs.DataArguments
         train_args: tuning.config.configs.TrainingArguments
+        train_control_args: configs.PTCArguments for controlling the training loop using policy rules
         peft_config: peft_config.LoraConfig for Lora tuning | \
         peft_config.PromptTuningConfig for prompt tuning | \
         None for fine tuning
@@ -222,7 +224,8 @@ def train(
     aim_callback = get_aimstack_callback()
     file_logger_callback = FileLoggingCallback(logger)
     peft_saving_callback = PeftSavingCallback()
-    callbacks = [aim_callback, peft_saving_callback, file_logger_callback]
+    ptc_callback = PolicyDrivenTrainerControl(train_control_args, train_args)
+    callbacks = [aim_callback, peft_saving_callback, file_logger_callback, ptc_callback]
 
     if train_args.packing:
         logger.info("Packing is set to True")
@@ -276,6 +279,7 @@ def main(**kwargs):
             configs.ModelArguments,
             configs.DataArguments,
             configs.TrainingArguments,
+            configs.PTCArguments,
             peft_config.LoraConfig,
             peft_config.PromptTuningConfig,
         )
@@ -290,6 +294,7 @@ def main(**kwargs):
         model_args,
         data_args,
         training_args,
+        trainer_control_args,
         lora_config,
         prompt_tuning_config,
         peft_method,
@@ -301,7 +306,7 @@ def main(**kwargs):
         tune_config = prompt_tuning_config
     else:
         tune_config = None
-    train(model_args, data_args, training_args, tune_config)
+    train(model_args, data_args, training_args, trainer_control_args, tune_config)
 
 
 if __name__ == "__main__":
